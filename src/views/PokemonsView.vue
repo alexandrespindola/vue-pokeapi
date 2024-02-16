3a809dfea5ee7e58e0d05da466e6adaa9b999699
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-02-16 20:46:08 +0100

    Atualiza tamanho do texto para PokemonsView.vue

--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -23,7 +23,7 @@ getData()
 <template>
     <div class="container flex flex-col gap-10">
     <h1 class="text-5xl font-semibold text-center">Pokemons</h1>
-    <p class="text-center">Click on a Pokemon to see more details</p>
+    <p class="text-center text-2xl">Click on a Pokemon to see more details</p>
       <div class="flex flex-row justify-center w-75 mx-auto">
         <ul class="flex flex-row justify-center gap-10 flex-wrap ">
           <li v-for="poke in pokemons" class="button">
