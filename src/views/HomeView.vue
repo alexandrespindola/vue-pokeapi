f201a7854a7642eb70a764c4df65ad54d1dd6bd2
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-02-27 13:00:01 +0100

    Adiciona dependência do Pinia e atualiza o contador na página inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <script setup>
+import { useCounterStore } from '@/store/counter.js'
+import { storeToRefs } from 'pinia';
+
+const useCounter = useCounterStore()
+
+const { increment } = useCounter
+
+const { double, count } = storeToRefs(useCounter)
 
 </script>
 
 <template>
   <h1 class="text-5xl font-bold">Vue PokeAPI</h1>
-  <RouterLink to="/pokemons" class="button transition-all ease-in-out duration-500 text-2xl">Click to see the Pokemons list</RouterLink>
+  <RouterLink to="/pokemons" class="button transition-all ease-in-out duration-500 text-2xl">Click to see the Pokemons
+    list</RouterLink>
+  <h2>Home double counter: {{ double }}</h2>
+  <h3>Home counter: {{ count }}</h3>
+  <button @click="increment">Increment</button>
 </template>
